5d43643971fce9ac1798e31d8396958992932711
Michael George <georgem@wlwv.k12.or.us>
2019-09-06 09:13:29 -0700

    added jabberwocky file

---
+++ Jabberwocky.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Jabberwocky here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Jabberwocky
+{
+    public static void main (String[] args){
+        System.out.print("'Twas Brilling, And The Slithy Toves/\n");
+        System.out.print("Did Gyre And Gimble In The Wabe:\n");
+        System.out.print("All Mimsy Were The \"Borogroves\"/\n");
+        System.out.print("And The Mome Raths Out Grabe.");
+    }
+}
