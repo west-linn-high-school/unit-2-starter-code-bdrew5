20d16c827dff1ac2e3b6c24f13bf42cc2baff908
Michael George <georgem@wlwv.k12.or.us>
2019-09-10 08:37:59 -0700

    Added Geometry

---
+++ Geometry.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Geometry here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Geometry
+{
+    public static void main(String[] args) {
+        int sides = 7;
+        int radius = 5;
+        final double PI = 3.14;
+        
+        System.out.println("A heptagon " +sides + " sides.");
+        
+        int sides = 10;
+        
+        System.out.println("A decagon has " +sides + " sides.");
+    }
+}
