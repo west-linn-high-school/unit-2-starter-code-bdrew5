3c6f9a46ca49e08a03e900d95b41323f41e3625a
Michael George <georgem@wlwv.k12.or.us>
2019-09-10 08:47:11 -0700

    Added Geomerty

--- Geometry.java
+++ Geometry.java
@@ -13,8 +13,8 @@ public class Geometry
         final double PI = 3.14;
         
         System.out.println("A heptagon " +sides + " sides.");
-        System.out.println("A decagon has " +sides + " sides.");
-        System.out.println("A dodecagon has " +sides + " sides.");
-        System.out.println("The circumference of a circle with a radius of " +radius+ "is " +PI);
+        System.out.println("A decagon has " +(sides+3) + " sides.");
+        System.out.println("A dodecagon has " +(sides+radius) + " sides.");
+        System.out.println("The circumference of a circle with a radius of " +radius+ " is " +PI);
     }
 }
